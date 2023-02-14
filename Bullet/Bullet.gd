921dacfe5a886a0a7dd7ce3c2c8e85d193a5ebe3
2680910 <2680910@schoolsnet.act.edu.au>
2023-02-15 10:26:53 +1100

    bullet

--- Bullet/Bullet.gd
+++ Bullet/Bullet.gd
@@ -13,10 +13,15 @@ func _ready():
 
 func _physics_process(delta):
 	var collidedObject = move_and_collide(Vector2(0, -speed*delta))
-	if (collidedObject):
-		
-		queue_free()
+
+func _process(delta):
+	if Input.is_action_just_pressed("fire"):
 		GlobalVariables.bulletInstanceCount -= 1 
+		var bulletInstance = bulletSource.instance()
+		bulletInstance.position = Vector2(position.x, position.y-20)
+		get_tree().get_root().add_child(bulletInstance)
+		
+
 
 
 
