6b5da9904fc4292f471da64b3c4d288b4d111a8e
2680910 <2680910@schoolsnet.act.edu.au>
2023-03-01 10:28:58 +1100

    automatticfiring

--- MainGame/MainGame.gd
+++ MainGame/MainGame.gd
@@ -7,3 +7,12 @@ var currentTimer
 func _ready():
 	currentTimer = countdownMax
 	$HUD/Countdown.text = str(currentTimer)
+	
+	while currentTimer > 0:
+		yield(get_tree().create_timer(1.0), "timeout")
+		$HUD/Countdown.text = str(currentTimer)
+		currentTimer = currentTimer - 1
+		print(currentTimer)
+	print("Skill Issue")
+	# Load the main menu again 
+	get_tree().change_scene("res://Menu/Menu.tscn")
