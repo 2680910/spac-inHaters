89db1061156f25c30fbb891636152b86469e5218
2680910 <2680910@schoolsnet.act.edu.au>
2023-03-24 12:14:21 +1100

    lop

--- MainGame/MainGame.gd
+++ MainGame/MainGame.gd
@@ -6,7 +6,7 @@ var currentTimer
 
 func _ready():
 	currentTimer = countdownMax 
-	$HUD/Countdown.text = str(currentTimer)
+	$HUD/Countdown.text = str(currentTimer) 
 	
 	while currentTimer > 0:
 		yield(get_tree().create_timer(1.0), "timeout")
