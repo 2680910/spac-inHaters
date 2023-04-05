9bf0c53f6c59fa5421d43c63c4c76bfa877f3f47
2680910 <2680910@schoolsnet.act.edu.au>
2023-04-05 10:32:24 +1000

    e

--- Menu/new_script.gd
+++ Menu/new_script.gd
@@ -8,9 +8,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-func 
-
+	pass # Replace
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
